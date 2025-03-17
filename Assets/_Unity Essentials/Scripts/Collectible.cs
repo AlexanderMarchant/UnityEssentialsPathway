a0bf1d6588473fa2fcab65264eaccdc436f7a180
Alex Marchant <alex_marchie@outlook.com>
2025-03-17 13:44:55 +0100

    Add collection support

--- Assets/_Unity Essentials/Scripts/Collectible.cs
+++ Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -1,8 +1,10 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
     public float rotationSpeed;
+    public GameObject onCollectEffect;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,5 +16,15 @@ public class Collectible : MonoBehaviour
     void Update()
     {
         transform.Rotate(0, rotationSpeed, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Destroy the collectible
+            Destroy(gameObject);
+            Instantiate(onCollectEffect, transform.position, transform.rotation);
+        }
     }
 }
