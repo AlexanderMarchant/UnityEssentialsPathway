a67cd85ddb04dbc17c178e674ff9b6408d6e643c
Alex Marchant <alex_marchie@outlook.com>
2025-03-17 13:18:00 +0100

    Add player character to scene

---
+++ Assets/_Unity Essentials/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 2.0f; // Set player's movement speed
+    public float rotationSpeed = 165.0f; // Set player's rotation speed
+
+    private Rigidbody rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Handle physics-based movement and rotation.
+    private void FixedUpdate()
+    {
+        // Move player based on vertical input.
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = transform.forward * moveVertical * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + movement);
+
+        // Rotate player based on horizontal input.
+        float turn = Input.GetAxis("Horizontal") * rotationSpeed * Time.fixedDeltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+        rb.MoveRotation(rb.rotation * turnRotation);
+    }
+}
